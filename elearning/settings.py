bd0f1c07ef407f1550fb25a8541a926ff6490adb
SoftwareTechnology-Hub <hii.intern.hii@gmail.com>
2025-10-22 18:17:46 +0530

    a

--- elearning/settings.py
+++ elearning/settings.py
@@ -116,13 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+#STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+#STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 AUTH_USER_MODEL = 'elearnapp.CustomUser'
@@ -131,7 +131,6 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 import os
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'elearnapp','static')]
