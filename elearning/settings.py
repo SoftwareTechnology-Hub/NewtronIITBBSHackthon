ca7ee3e935cbe4ab2e41a90bdd5c459b55363eed
SoftwareTechnology-Hub <hii.intern.hii@gmail.com>
2025-10-22 16:36:20 +0530

    a

--- elearning/settings.py
+++ elearning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-m80hc=urn-m*tm!&ro%o&*&cff39h%k(g0ekw3k%zhxwhf^3no
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
